b4b9d97ef40a3c5ee4a6bd967b9c3b74f256e618
minhdq98 <dquangminh98@gmail.com>
2020-11-23 17:01:45 +0700

    stylingCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Column from './components/template/Column'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const TrelloClone = () => (
+    <div>
+        <h3>Trello Clone</h3>
+        <div className="container trello-clone-container">
+            <Column />
+        </div>
     </div>
-  );
-}
+)
 
-export default App;
+const App = () => (
+    <div>
+        <TrelloClone />
+    </div>
+)
+
+export default App
---
+++ src/components/template/Column.js
@@ -0,0 +1,15 @@
+const Column = () => (
+    <div className="column">
+        <div className="header"><b>Phone Features</b></div>
+        <div className="card-list">
+            <div className="card">Random Card</div>
+            <div className="card">Random Card</div>
+            <div className="card">Random Card</div>
+        </div>
+        <div className="footer">
+            <i className="fa fa-plus"></i> Add another card
+        </div>
+    </div>
+)
+
+export default Column
